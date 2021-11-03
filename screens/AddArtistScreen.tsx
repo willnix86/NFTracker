88387a9505b17fe8568b3579cee58b236cf85dcf
Will Nixon <will_nixon@me.com>
2021-11-03 14:07:07 -0500

    rename folder

--- screens/AddArtistScreen.tsx
+++ screens/AddArtistScreen.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import AddArtistForm from '../components/forms/AddArtistForm';
+import AddArtistForm from '../components/modals/AddArtistForm';
 import Modal from '../components/Modal';
 
 export default function AddArtistScreen() {
